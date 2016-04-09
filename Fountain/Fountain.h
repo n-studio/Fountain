ae2dab230e74a552f06a0f1281dbc2a024fa4244
Matthew Nguyen <contact@n-studio.fr>
2016-04-10 03:14:17 +0900

    Framework now compatible osx

---
+++ Fountain/Fountain.h
@@ -0,0 +1,17 @@
+//
+//  Fountain.h
+//  Fountain
+//
+//  Created by Lidner on 4/10/16.
+//  Copyright © 2016 Nima Yousefi. All rights reserved.
+//
+
+//! Project version number for Fountain.
+FOUNDATION_EXPORT double FountainVersionNumber;
+
+//! Project version string for Fountain.
+FOUNDATION_EXPORT const unsigned char FountainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fountain/PublicHeader.h>
+
+
